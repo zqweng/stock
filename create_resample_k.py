8dbf85a9f6c9ca79c392e25274217278284d666e
zqweng <53497427+zqweng@users.noreply.github.com>
2019-08-20 00:04:47 -0700

    update commit

--- create_resample_k.py
+++ create_resample_k.py
@@ -8,32 +8,32 @@ def create_resample_k(stock_list_file, start_date, end_date, resample_intv):
 
     df = pd.read_csv(stock_list_file, converters={'code': lambda x: str(x)})
 
-    tick_dir = Path().joinpath('..', '..', 'stockdata')
+    tick_dir = Path().joinpath('..', '..', 'stockdata', 'tick')
     timeRange = pd.date_range(start=start_date, end=end_date).strftime('%Y-%m-%d')
 
-    result_dir = os.path.join(tick_dir, resample_intv)
+    result_dir = os.path.join('..', '..', 'stockdata', resample_intv)
     if not os.path.exists(result_dir):
         os.mkdir(result_dir)
 
     for row in df.itertuples():
         dir_path = os.path.join(tick_dir, row.code)
         if not os.path.exists(dir_path):
-            print('Directory', os.path.join(tick_dir, row.code), 'not exist')
+            print('Directory', dir_path, 'not exist')
             quit()
 
         df_resample_k = pd.DataFrame()
 
         for date_val in timeRange:
-            file_name = os.path.join(tick_dir, row.code, date_val + '.csv')
+            file_name = os.path.join(dir_path, date_val + '.csv')
             if os.path.exists(file_name):
                 df_tick = pd.read_csv(file_name)
                 if not df_tick.empty:
                     df_tick = mylib.resample(df_tick, resample_intv, date_val)
                 df_resample_k = pd.concat([df_resample_k,df_tick])
 
-        result_file_name = os.path.join(result_dir, row.code + '_' + resample_intv + '.csv')
+        result_file_name = os.path.join(result_dir, row.code + '.csv')
         print ('write result file', result_file_name)
         df_resample_k.to_csv(result_file_name)
 
 if __name__ == "__main__":
-    create_resample_k('mystocklist-detail.csv', '2019-01-01', '2019-06-1','60min')
+    create_resample_k('mystocklist-detail.csv', '2019-07-08', '2019-07-30','60min')
--- driver3.py
+++ driver3.py
@@ -14,8 +14,24 @@ result_string = ''
 stock_list_file = 'mystocklist-detail.csv'
 #stock_list_file = 'test_list.csv'
 
-
+"""
 history_dir = Path().joinpath('..', '..', 'stockdata','day')
+#update_history.load_history(history_dir, stock_list_file)
+
+df = pd.read_csv(Path().joinpath(stock_list_file), converters={'code': lambda x: str(x)})
+
+if df.empty:
+    print('no stock list, quit')
+
+result_df3 = mylib.day_find_up_period_list(df, history_dir, 120)
+if not result_df3.empty:
+    result_string = result_string + '\n\nprice has 3 days up between 1% and 4% \n' + result_df3.to_string()
+else:
+    result_string = result_string + '\n\nno price has 3 days up between 1% and 4% \n'
+print(result_string)
+"""
+
+history_dir = Path().joinpath('..', '..', 'stockdata','60min')
 #update_history.load_history(history_dir, stock_list_file)
 
 df = pd.read_csv(Path().joinpath(stock_list_file), converters={'code': lambda x: str(x)})
