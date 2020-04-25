c7992431e93bf06459f4820e4f078163af7c64dc
johnny <johnny.weng8@gmail.com>
2020-04-24 20:33:01 -0700

    update

--- qt-learning.py
+++ qt-learning.py
@@ -2,7 +2,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 import sys
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import numpy as np
+import pdb
 
 
 class Window(QMainWindow):
@@ -21,22 +23,27 @@ class Window(QMainWindow):
         self.MyUI()
 
     def MyUI(self):
-        canvas = Canvas(self, width=8, height=4)
-        canvas.move(0, 0)
+        self.canvas = Canvas(self, width=8, height=4)
+        self.canvas.move(0, 0)
 
-        button = QPushButton("Click Me", self)
+        button = QPushButton("向前", self)
         button.move(100, 450)
+        button.clicked.connect(self.slot_method)
 
-        button2 = QPushButton("Click Me Two", self)
+        button2 = QPushButton("向后", self)
         button2.move(250, 450)
 
+    def slot_method(self):
+        self.canvas.clear_fig()
+        self.canvas.plot_next()
+        self.canvas.draw()
 
 class Canvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=5, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axe = self.fig.subplots()
 
-        FigureCanvas.__init__(self, fig)
+        FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
 
         self.plot()
@@ -44,8 +51,16 @@ class Canvas(FigureCanvas):
     def plot(self):
         x = np.array([50, 30, 40])
         labels = ["Apples", "Bananas", "Melons"]
-        ax = self.figure.add_subplot(111)
-        ax.pie(x, labels=labels)
+        self.axe.pie(x, labels=labels)
+
+    def plot_next(self):
+        x = np.array([5, 3, 40])
+        labels = ["Apples", "Bananas", "Meeeeeeee"]
+        self.axe.pie(x, labels=labels)
+
+    def clear_fig(self):
+        self.fig.clf()
+        self.axe = self.fig.subplots()
 
 
 app = QApplication(sys.argv)
